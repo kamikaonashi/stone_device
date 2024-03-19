19c302e7064e367ac90ba18cd3bf580dc5c65adf
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2024-03-19 18:46:49 +0100

    stone: enable pelt8 during boot
    The default PELT half-life is 32ms. Change it to 8ms.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,7 @@ BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += pelt=8
 
 # Media
 TARGET_USES_ION := true
